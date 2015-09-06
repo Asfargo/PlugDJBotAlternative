b5e7649a8a56a4b1939679ee52dcaa13135313b1
Asfargo <Asfargo@semEmail.com>
2015-09-06 01:30:05 -0300

    Update Citartodos.js

--- Citartodos.js
+++ Citartodos.js
@@ -17,6 +17,36 @@ function chat(data) {
         API.chatLog("Iniciar05");
 
         
+
+API.on(API.CHAT, chat);
+function chat(data) {
+        
+        var waitlist = API.getUsers();
+        if(data.message.indexOf("Iniciar03") > -1)
+        {
+            API.chatLog("Iniciar0X (Se for flood X = 3) " + API.getDJ().username, false);
+
+
+            {
+                API.sendChat("Hello @" + waitlist[0].username + " @" + waitlist[1].username + " @" + waitlist[2].username + " @" + waitlist[3].username + " @" + waitlist[4].username + " @" + waitlist[5].username + " @" + waitlist[6].username + " @" + waitlist[7].username + " @" + waitlist[8].username + " @" + waitlist[9].username + " @" + waitlist[10].username + " @" + waitlist[11].username + " @" + waitlist[12].username + " @" + waitlist[13].username + " @" + waitlist[14].username + " @" + waitlist[15].username + " @" + waitlist[16].username + " @" + waitlist[17].username + " @" + waitlist[18].username + " @" + waitlist[19].username + " @" + waitlist[20].username + " @" + waitlist[21].username, false);
+            }
+
+            API.chatLog("", false);
+        }
+        }
+        API.chatLog("Iniciar03");
+
+
+
+
+
+
+
+
+
+
+// Original
+
 API.on(API.CHAT, chat);
 function chat(data) {
         
@@ -36,20 +66,3 @@ function chat(data) {
         }
         }
         
-API.on(API.CHAT, chat);
-function chat(data) {
-        
-        var waitlist = API.getUsers();
-        if(data.message.indexOf("Iniciar03") > -1)
-        {
-            API.chatLog("Iniciar0X (Se for flood X = 3) " + API.getDJ().username, false);
-
-
-            {
-                API.sendChat("Hello @" + waitlist[0].username + " @" + waitlist[1].username + " @" + waitlist[2].username + " @" + waitlist[3].username + " @" + waitlist[4].username + " @" + waitlist[5].username + " @" + waitlist[6].username + " @" + waitlist[7].username + " @" + waitlist[8].username + " @" + waitlist[9].username + " @" + waitlist[10].username + " @" + waitlist[11].username + " @" + waitlist[12].username + " @" + waitlist[13].username + " @" + waitlist[14].username + " @" + waitlist[15].username + " @" + waitlist[16].username + " @" + waitlist[17].username + " @" + waitlist[18].username + " @" + waitlist[19].username + " @" + waitlist[20].username + " @" + waitlist[21].username, false);
-            }
-
-            API.chatLog("", false);
-        }
-        }
-        API.chatLog("Iniciar03");
