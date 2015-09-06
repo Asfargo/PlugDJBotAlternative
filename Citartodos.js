50ff17ba5d2cdbf98f57b87b20b27229f0114c86
Asfargo <Asfargo@semEmail.com>
2015-09-05 23:14:14 -0300

    Update Citartodos.js

--- Citartodos.js
+++ Citartodos.js
@@ -55,3 +55,21 @@ function chat(data) {
         }
         }
         
+API.on(API.CHAT, chat);
+function chat(data) {
+        
+        var waitlist = API.getWaitList();
+        if(data.message.indexOf("atoa") > -1)
+        {
+            API.chatLog(" ", false);
+            API.chatLog("Automated message: Waitlist: ", false);
+            API.chatLog("Hi @" + API.getDJ().username, false);
+
+
+            {
+                API.sendChat("Ola, desde já lhes peço desculpas @" + waitlist[1].username + " @" + waitlist[2].username + " @" + waitlist[3].username + " @" + waitlist[4].username + " @" + waitlist[5].username + " @" + waitlist[6].username + " @" + waitlist[7].username + " @" + waitlist[8].username + " @" + waitlist[9].username, false);
+            }
+
+            API.chatLog(" ", false);
+        }
+        }
