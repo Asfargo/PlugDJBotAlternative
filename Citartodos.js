1f13ab424a24c64c861b931eabbdf821e1120494
Asfargo <Asfargo@semEmail.com>
2015-09-05 23:03:23 -0300

    Update Citartodos.js

--- Citartodos.js
+++ Citartodos.js
@@ -17,3 +17,22 @@ function chat(data) {
         }
         }
         
+API.on(API.CHAT, chat);
+function chat(data) {
+        
+        var waitlist = API.getWaitList();
+        if(data.message.indexOf("0151") > -1)
+        {
+            API.chatLog(" ", false);
+            API.chatLog("Automated message: Waitlist: ", false);
+            API.chatLog("Hi @" + API.getDJ().username, false);
+
+
+            {
+                API.chatLog("Automated message: @" + waitlist[1].username + " @" + waitlist[2].username + " @" + waitlist[3].username + " @" + waitlist[4].username + " @" + waitlist[5].username + " @" + waitlist[6].username + " @" + waitlist[7].username + " @" + waitlist[8].username + " @" + waitlist[9].username + " @" + waitlist[10].username + " @" + waitlist[11].username + " @" + waitlist[12].username, false);
+            }
+
+            API.chatLog(" ", false);
+        }
+        }
+        
