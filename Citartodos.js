b458be3d62fd69d32f77c82a95dbfc4f629f0e16
Asfargo <Asfargo@semEmail.com>
2015-09-05 23:51:43 -0300

    Update Citartodos.js

--- Citartodos.js
+++ Citartodos.js
@@ -2,30 +2,15 @@ API.on(API.CHAT, chat);
 function chat(data) {
         
         var waitlist = API.getWaitList();
-        if(data.message.indexOf("@djs") > -1)
-        {
-            API.chatLog("Hi @" + API.getDJ().username, false);
-
-            {
-                API.sendChat("Ola @" + waitlist[1].username + " @" + waitlist[2].username + " @" + waitlist[3].username + " @" + waitlist[4].username + " @" + waitlist[5].username + " @" + waitlist[6].username + " @" + waitlist[7].username + " @" + waitlist[8].username + " @" + waitlist[9].username + " @" + waitlist[10].username + " @" + waitlist[11].username + " @" + waitlist[12].username + " @" + waitlist[13].username + " @" + waitlist[14].username + " @" + waitlist[15].username + " @" + waitlist[16].username + " @" + waitlist[17].username + " @" + waitlist[18].username + " @" + waitlist[19].username + " @" + waitlist[0].username, false);
-            }
-
-        }
-        }
-        
-API.on(API.CHAT, chat);
-function chat(data) {
-        
-        var waitlist = API.getWaitList();
         if(data.message.indexOf("0151") > -1)
         {
             API.chatLog(" ", false);
-            API.chatLog("Automated message: Waitlist: ", false);
+            API.chatLog("Automated message Waitlist! ", false);
             API.chatLog("Hi @" + API.getDJ().username, false);
 
 
             {
-                API.sendChat("Ola @" + waitlist[1].username + " @" + waitlist[2].username + " @" + waitlist[3].username + " @" + waitlist[4].username + " @" + waitlist[5].username + " @" + waitlist[6].username + " @" + waitlist[7].username + " @" + waitlist[8].username + " @" + waitlist[9].username + " @" + waitlist[10].username + " @" + waitlist[11].username + " @" + waitlist[12].username + " @" + waitlist[13].username + " @" + waitlist[14].username + " @" + waitlist[15].username + " @" + waitlist[16].username + " @" + waitlist[17].username + " @" + waitlist[18].username + " @" + waitlist[19].username + " @" + waitlist[0].username, false);
+                API.sendChat("Hello @" + waitlist[0].username + " @" + waitlist[1].username + " @" + waitlist[2].username + " @" + waitlist[3].username + " @" + waitlist[4].username + " @" + waitlist[5].username + " @" + waitlist[6].username + " @" + waitlist[7].username + " @" + waitlist[8].username + " @" + waitlist[9].username + " @" + waitlist[10].username + " @" + waitlist[11].username + " @" + waitlist[12].username + " @" + waitlist[13].username + " @" + waitlist[14].username + " @" + waitlist[15].username + " @" + waitlist[16].username + " @" + waitlist[17].username + " @" + waitlist[18].username + " @" + waitlist[19].username + " @" + waitlist[20].username + " @" + waitlist[21].username, false);
             }
 
             API.chatLog(" ", false);
