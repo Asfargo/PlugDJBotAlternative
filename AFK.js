cf77cebe0f78e1cb86d8a03053083108d8e8a041
Asfargo <Asfargo@semEmail.com>
2015-09-08 14:32:21 -0300

    Update AFK.js

--- AFK.js
+++ AFK.js
@@ -2,7 +2,7 @@ API.on(API.CHAT, chat);
 function chat(data) {
         var role = API.getUser(data.fromID).permission;
         botrole = API.getUser().permission;
-        if(data.message.indexOf("@Asfargo R.", "Avagon") > -1)
+        if(data.message.indexOf(["@Asfargo R.", "Avagon"]) > -1)
         {
                 API.moderateDeleteChat(data.cid);
                 API.sendChat(":warning: @" + data.un + " I'm AFK. / Estou ausente. :sweat_smile:");
